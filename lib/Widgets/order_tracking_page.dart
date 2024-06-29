c7543900055d5d8fe1242bcd978312383daa6f1f
IgnacioBello1 <ibello@miuandes.cl>
2024-06-29 11:43:13 -0400

    init

--- lib/Widgets/order_tracking_page.dart
+++ lib/Widgets/order_tracking_page.dart
@@ -73,25 +73,64 @@ class OrderTrackingPageState extends State<OrderTrackingPage> {
     // Add markers to the set with associated information
     _markers.add(
       Marker(
-        markerId: MarkerId('marker1'),
+        markerId: MarkerId('Punto Limpio1'),
         position: LatLng(-33.36413955292655, -70.51871732100369),
         // Custom icon for the marker
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-        onTap: () => _onMarkerTapped(MarkerId('marker1')),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+        onTap: () => _onMarkerTapped(MarkerId('Punto Limpio1')),
       ),
     );
-    _markerInfo[MarkerId('marker1')] = 'Information for Marker 1';
+    _markerInfo[MarkerId('Punto Limpio1')] = 'Punto Limpio, Av. Raúl Labbé 12.099, 7690554 Lo Barnechea, Región Metropolitana';
 
     _markers.add(
       Marker(
-        markerId: MarkerId('marker2'),
+        markerId: MarkerId('Reciclaje Botellas Vidrio'),
         position: LatLng(-33.338572320718775, -70.5434051377558),
         icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
-        onTap: () => _onMarkerTapped(MarkerId('marker2')),
+        onTap: () => _onMarkerTapped(MarkerId('Reciclaje Botellas Vidrio')),
       ),
     );
-    _markerInfo[MarkerId('marker2')] = 'Information for Marker 2';
+    _markerInfo[MarkerId('Reciclaje Botellas Vidrio')] = 'Reciclaje Botellas Vidrio, Cam. Los Trapenses 3515, 7700148 Lo Barnechea, Región Metropolitana';
 
+    _markers.add(
+      Marker(
+        markerId: MarkerId('Punto Limpio TriCiclos'),
+        position: LatLng(-33.38725164393464, -70.49695752663585),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+        onTap: () => _onMarkerTapped(MarkerId('Punto Limpio TriCiclos')),
+      ),
+    );
+    _markerInfo[MarkerId('Punto Limpio TriCiclos')] = 'Punto Limpio TriCiclos, Universidad del Desarrollo - UDD - Av Plaza 680, 7610615 Las Condes, Región Metropolitana';
+
+     _markers.add(
+      Marker(
+        markerId: MarkerId('Punto Verde'),
+        position: LatLng(-33.39867865294377, -70.58208427419848),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+        onTap: () => _onMarkerTapped(MarkerId('Punto Verde')),
+      ),
+    );
+    _markerInfo[MarkerId('Punto Verde')] = 'Punto Verde, Cerro Colorado 5051, 7560995 Las Condes, Región Metropolitana';
+
+    _markers.add(
+      Marker(
+        markerId: MarkerId('Punto Limpio2'),
+        position: LatLng(-33.40383777894912, -70.56869468732114),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+        onTap: () => _onMarkerTapped(MarkerId('Punto Limpio2')),
+      ),
+    );
+    _markerInfo[MarkerId('Punto Limpio2')] = 'Punto Limpio, Las Condes, Región Metropolitana';
+
+    _markers.add(
+      Marker(
+        markerId: MarkerId('Centro de reciclaje recoleta'),
+        position: LatLng(-33.39323257651344, -70.64147910829533),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+        onTap: () => _onMarkerTapped(MarkerId('Centro de reciclaje recoleta')),
+      ),
+    );
+    _markerInfo[MarkerId('Centro de reciclaje recoleta')] = 'Centro de reciclaje recoleta, 8440697 Recoleta, Región Metropolitana';
     // Set the state to update the map with the newly added markers
     setState(() {});
   }
@@ -111,7 +150,7 @@ class OrderTrackingPageState extends State<OrderTrackingPage> {
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: Text('Marker Info'),
+            title: Text('Info'),
             content: Text(info),
             actions: <Widget>[
               TextButton(
